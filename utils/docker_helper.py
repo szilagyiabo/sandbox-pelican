499bfac6b09f7ba338ac4a8051f0c456482c5a76
Szilagyi Abraham <szilagyiabo@gmail.com>
2016-07-05 20:08:12 +0200

    Pull image if not exist

--- utils/docker_helper.py
+++ utils/docker_helper.py
@@ -4,17 +4,21 @@ import dockerpty
 docker_client = Client(base_url='unix://var/run/docker.sock')
 
 
-def sandbox_file_in_container(container, command, file_path):
+def sandbox_file_in_container(image, command, file_path):
+    if not docker_client.images(name=image):
+        """empty"""
+        docker_client.pull(image)
+
     directory = _find_dir(str(file_path))
-    container = docker_client.create_container(image=container, working_dir=directory,
-                                               volumes=[directory],
-                                               host_config=docker_client.create_host_config(binds=[
+    image = docker_client.create_container(image=image, working_dir=directory,
+                                           volumes=[directory],
+                                           host_config=docker_client.create_host_config(binds=[
                                                    directory + ':' + directory,
                                                ]),
-                                               stdin_open=True, tty=True,
-                                               command=command)
-    dockerpty.start(docker_client, container)
-    docker_client.remove_container(container)
+                                           stdin_open=True, tty=True,
+                                           command=command)
+    dockerpty.start(docker_client, image)
+    docker_client.remove_container(image)
 
 
 def _find_dir(path_to_file):
